a33d4fc2ce4b1a42d00ba6040fcc419569cb0c8a
xiaoxiao-pei <pxtoday@hotmail.com>
2022-12-02 14:58:03 -0500

    initial structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,19 @@ import Login from './components/Login';
 import Registration from './components/Registration';
 
 
-
+//administrator login statue
 export const AdminLoggedInContext = React.createContext({
   isAdminLoggedIn: false,
   setIsAdminLoggedIn: () => { },
 });
 
+//user login statue
+export const UserLoggedInContext = React.createContext({
+  isUserLoggedIn: false,
+  setIsUserLoggedIn: () => { },
+});
+
+//display mode(dark/light)
 export const ModeContext = React.createContext({
   mode: "light",
   setMode: () => { },
@@ -25,28 +32,33 @@ export const ModeContext = React.createContext({
 function App() {
 
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [mode, setMode] = useState("light");
-  const AdminloggedInProvide = [isAdminLoggedIn, setIsAdminLoggedIn]; // So we can pass down both value and setter
+
+
+  const adminLoggedInProvide = [isAdminLoggedIn, setIsAdminLoggedIn];
+  const userLoggedInProvide = [isUserLoggedIn, setIsUserLoggedIn];
   const modeProvide = [mode, setMode];
 
   return (
-  <div>
-    <modeProvide.Provider value={modeProvide}>
-      <AdminLoggedInContext.Provider value={AdminloggedInProvide}>
-        <Routes>
-          <Route path="/" element={<MainLayout />} >
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="register" element={<Registration />} />
-            <Route path="signin" element={<Login />} />
-            <Route path="*" element={<p>Invalid URL</p>} />
-          </Route>
-        </Routes>
+    <div>
+      <ModeContext.Provider value={modeProvide}>
+        <AdminLoggedInContext.Provider value={adminLoggedInProvide}>
+          <UserLoggedInContext.Provider value={userLoggedInProvide}>
+            <Routes>
+              <Route path="/" element={<MainLayout />} >
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="register" element={<Registration />} />
+                <Route path="signin" element={<Login />} />
+                <Route path="*" element={<p>Invalid URL</p>} />
+              </Route>
+            </Routes>
+          </UserLoggedInContext.Provider>
+        </AdminLoggedInContext.Provider>
+      </ModeContext.Provider>
 
-      </AdminLoggedInContext.Provider>
-    </modeProvide.Provider>
-
-  </div>)
+    </div>)
 
 }
 
