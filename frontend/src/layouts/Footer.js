bf16d8fceeb7d7dc5e584a1699d09e9e30b3db5a
wuweisz2021 <wuweisz2021@gmail.com>
2022-12-03 10:55:02 -0500

    update footer 1

--- frontend/src/layouts/Footer.js
+++ frontend/src/layouts/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
     return(
         <footer>
-            <h5> Copyright &copy; Xiao Pei</h5>
+            <h5> &copy;Copyright by Reacter      
+    &emsp;&emsp; Address:1000 Rue LakeShore Road, Sainte, QC,H9X 3L9, Canand      
+    &emsp;&emsp; Phone: (514)123-4567    
+    &emsp;&emsp; Email: reacter@gmail.com</h5>
         </footer>
     )
 };
