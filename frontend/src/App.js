7141f57de999d70c6d3169c0ae7d6b220ca8df94
Zhang Xiao <zxiao0628@gmail.com>
2022-12-07 15:08:55 -0500

    add log out is time out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import AddPost from "./pages/AddPost.js";
 import MainLayout from "./layouts/MainLayout";
 import UserLayout from "./layouts/UserLayout";
 import AdminLayout from "./layouts/AdminLayout";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PostsUser from "./pages/Posts";
 import PostsAdmin from "./pages/Posts";
 
@@ -31,72 +31,80 @@ export const ReactContext = React.createContext({
 function App() {
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-
+  const [isTimeOut, setIsTimeOut] = useState(false);
   const [startTime, setStartTime] = useState();
 
-  // const user = localStorage.getItem('user')
+  const user = localStorage.getItem("user");
 
-  // const mouseMoveHandle = () => {
-  //   if (user) setStartTime(new Date().getTime());
-  // };
+  const mouseMoveHandle = () => {
+    if (user) setStartTime(new Date().getTime());
+  };
 
   const navigate = useNavigate();
-  // count down to rediret user to home page if no action during long time
-  // useEffect(() => {
-  //   if (user) {
-  //     console.log("start counting down");
-  //     const check = setInterval(() => {
-  //       const timeDiff = new Date().getTime() - startTime;
-  //       if (timeDiff >= 10 * 10 * 1000) {
-  //         navigate("/timeout");
-  //         localStorage.removeItem("user");
 
-  //         return;
-  //       }
-  //       console.log("time left:" + timeDiff);
-  //     }, 1000);
-  //     return () => {
-  //       clearInterval(check);
-  //     };
-  //   }
-  // }, [startTime]);
+  useEffect(() => {
+    if (user) {
+      console.log("start counting down");
+      const check = setInterval(() => {
+        const timeDiff = new Date().getTime() - startTime;
+        if (timeDiff >= 10 * 10 * 1000) {
+          navigate("/Login");
+          localStorage.removeItem("user");
+          setIsTimeOut(true);
+          return;
+        }
+        console.log("time left:" + timeDiff);
+      }, 1000);
+      return () => {
+        clearInterval(check);
+      };
+    }
+  }, [startTime]);
 
   const contextProvide = [
     isAdminLoggedIn,
     setIsAdminLoggedIn,
     isUserLoggedIn,
     setIsUserLoggedIn,
+    isTimeOut,
+    setIsTimeOut,
   ];
 
-  // console.log(recAuthors);
-  // onMouseMove={mouseMoveHandle}
   return (
-    <ReactContext.Provider value={contextProvide}>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="register" element={<Registration />} />
-          <Route path="login" element={<Login />} />
-          <Route path="recAuthorPosts/:userId/:userName" element={<Posts />} />
+    <div onMouseMove={mouseMoveHandle}>
+      <ReactContext.Provider value={contextProvide}>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="register" element={<Registration />} />
+            <Route path="login" element={<Login />} />
+            <Route
+              path="recAuthorPosts/:userId/:userName"
+              element={<Posts />}
+            />
 
-          <Route path="/user" element={<UserLayout />}>
-            <Route index element={<Profile />} />
-            <Route path="posts" element={<PostsUser />} />
-            <Route path="add" element={<AddPost />} />
-          </Route>
+            <Route path="/user" element={<UserLayout />}>
+              <Route index element={<Profile />} />
+              <Route path="posts" element={<PostsUser />} />
+              <Route path="add" element={<AddPost />} />
+            </Route>
 
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Profile />} />
-            <Route path="posts" element={<PostsAdmin />} />
-            <Route path="authors" element={<Authors />} />
-            <Route path="author/profile/:userId" element={<Profile />} />
-            <Route path="author/posts/:userId/:userName" element={<Posts />} />
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<Profile />} />
+              <Route path="posts" element={<PostsAdmin />} />
+              <Route path="authors" element={<Authors />} />
+              <Route path="author/profile/:userId" element={<Profile />} />
+              <Route
+                path="author/posts/:userId/:userName"
+                element={<Posts />}
+              />
+            </Route>
+            <Route path="*" element={<p>Invalid URL</p>} />
           </Route>
-          <Route path="*" element={<p>Invalid URL</p>} />
-        </Route>
-      </Routes>
-    </ReactContext.Provider>
+        </Routes>
+      </ReactContext.Provider>
+    </div>
   );
 }
 
